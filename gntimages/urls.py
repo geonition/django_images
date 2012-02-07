22b140cfd7dfa91b808d6458c805e52d4a208eb9
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2012-02-07 15:20:03 +0200

    the area icons work now

--- gntimages/urls.py
+++ gntimages/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import url
 
 urlpatterns = patterns('gntimages.views',
-            #test templates
             url(r'^needle',
                 'needle',
                 name='image_needle'),
-            #test templates
             url(r'^route',
                 'route',
                 name='image_route'),
+            url(r'^area',
+                'area',
+                name='image_area'),
         )
--- gntimages/views.py
+++ gntimages/views.py
@@ -90,37 +90,33 @@ def area(request):
     red = int(color[0:2], 16)
     green = int(color[2:4], 16)
     blue = int(color[4:6], 16)
-    new_color = (red, green, blue)
+    new_color = (red, green, blue, 255)
     new_scale = float(request.GET.get('scale', '1.0'))
 
     #requires the images to be in MEDIA_ROOT
     base_img = Image.open('%s/%s' % (settings.MEDIA_ROOT,
-                                    'base_route.png'))
-    color_img = Image.open('%s/%s' % (settings.MEDIA_ROOT,
-                                    'route_mask.png'))
+                                    'base_area.png'))
+        
+    new_image = Image.new('RGBA',
+                          (base_img.size[0],
+                           base_img.size[1]),
+                          new_color)
     base_pixels = base_img.load()
-    color_pixels = color_img.load()
-
-    #change the color
-    for y in xrange(color_img.size[1]):
-        for x in xrange(color_img.size[0]):
-            new_color = (new_color[0], new_color[1], new_color[2], 0)
-            color_pixels[x,y] = new_color
-
-    new_image = color_img.copy()
     new_img_data = new_image.load()
-    for y in xrange(color_img.size[1]):
-        for x in xrange(color_img.size[0]):
-            new_red = color_pixels[x, y][0]
-            new_green = color_pixels[x, y][1]
-            new_blue = color_pixels[x, y][2]
-            new_alpha = base_pixels[x, y][3]
-            new_img_data[x, y] = (int(new_red), int(new_green), int(new_blue), int(new_alpha))
+    a = 20
+    for y in xrange(new_image.size[1]):
+        for x in xrange(new_image.size[0]):
+            new_red = new_img_data[x, y][0] - base_pixels[x, y][0] + a
+            new_green = new_img_data[x, y][1] - base_pixels[x, y][0] + a
+            new_blue = new_img_data[x, y][2] - base_pixels[x, y][0] + a
+            new_alpha = base_pixels[x, y][1]
+            new_img_data[x, y] = (int(new_red),
+                                  int(new_green),
+                                  int(new_blue),
+                                  int(new_alpha))
 
     new_image = new_image.resize((int(new_image.size[0] * new_scale), int(new_image.size[1] * new_scale)))
-
-    new_image = new_image.resize((int(new_image.size[0] * new_scale), int(new_image.size[1] * new_scale)))
-
+    
     #this is not efficient, might need improvement
     response = HttpResponse(content_type="image/png")
     new_image.save(response, "PNG")
