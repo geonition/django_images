0e5e6b805283e4fda3c83374765505f10e8d694d
Kristoffer Snabb <ksnabb@ubuntu-virtual.(none)>
2012-04-26 12:18:22 +0300

    baseline version 4.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='gntimages',
-    version='0.1',
+    version='4.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_images',
     packages=find_packages(),
